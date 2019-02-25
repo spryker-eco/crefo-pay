95c9ec8ba5be8f4e4bd362cf0bb13b53cc20d290
danil.moroz <danil.moroz@spryker.com>
2019-02-25 18:08:04 +0200

    ECO-2300: Add js verification for payment on form submit event

--- src/SprykerEco/Yves/CrefoPay/Theme/default/components/molecules/crefopay-form-loader/crefopay-form-loader.ts
+++ src/SprykerEco/Yves/CrefoPay/Theme/default/components/molecules/crefopay-form-loader/crefopay-form-loader.ts
@@ -16,14 +16,29 @@ export default class CrefopayFormLoader extends Component {
     protected crefoPayScriptLoader: ScriptLoader;
     public secureFieldsClient: any;
     protected configuration: object;
+    protected paymentForm: HTMLFormElement;
+    protected paymentFormSubmitButton: HTMLButtonElement;
+    protected paymentInstrumentId: HTMLInputElement;
+    protected errorBlock: HTMLElement;
 
     protected readyCallback(): void {
+        this.paymentForm = <HTMLFormElement>document.querySelector(this.paymentFormSelector);
+        this.paymentFormSubmitButton = <HTMLButtonElement>this.paymentForm.querySelector('button[type="submit"]');
         this.crefoPayScriptLoader = <ScriptLoader>this.querySelector(`.${this.jsName}__script-loader`);
+        this.paymentInstrumentId = <HTMLInputElement>this.querySelector(this.paymentInstrumentIdSelector);
+        this.errorBlock = <HTMLElement>this.querySelector(`.${this.jsName}__error`);
+
         this.mapEvents();
     }
 
     protected mapEvents(): void {
         this.crefoPayScriptLoader.addEventListener('scriptload', () => this.onScriptLoad());
+        this.paymentFormSubmitButton.addEventListener('click', (event: Event) => this.onSubmitButtonClick(event));
+    }
+
+    protected onSubmitButtonClick(event: Event): void {
+        event.preventDefault();
+        this.secureFieldsClient.registerPayment();
     }
 
     protected onScriptLoad(): void {
@@ -31,20 +46,21 @@ export default class CrefopayFormLoader extends Component {
             new SecureFieldsClient(
                 this.crefopayShopPublicKey,
                 this.crefopayOrderId,
-                this.paymentRegisteredCallback,
+                this.paymentRegisteredCallback.bind(this),
                 this.initializationCompleteCallback,
                 CREFO_PAY_CONFIG);
     }
 
-    protected paymentRegisteredCallback(response) {
+    protected paymentRegisteredCallback(response): void {
         if (response.resultCode === 0) {
-            // Successful registration, continue to next page using JavaScript
+            this.paymentInstrumentId.value = response.paymentInstrumentId;
+            this.paymentForm.submit();
         } else {
-            // Error during registration, check the response for more details and dynamically show a message for the customer
+            this.errorBlock.classList.remove(this.classToToggle);
         }
     }
 
-    protected initializationCompleteCallback(response) {
+    protected initializationCompleteCallback(response): void {
         if (response.resultCode === 0) {
             // Successful registration, continue to next page using JavaScript
         } else {
@@ -58,5 +74,17 @@ export default class CrefopayFormLoader extends Component {
 
     get crefopayOrderId() {
         return this.getAttribute('order-id');
+    }
+
+    get paymentFormSelector() {
+        return this.getAttribute('payment-form-selector');
+    }
+
+    get paymentInstrumentIdSelector() {
+        return this.getAttribute('payment-instrument-id-selector');
+    }
+
+    get classToToggle() {
+        return this.getAttribute('class-to-toggle');
     }
 }
