3955134b08eebd576160ad42e9013bb52ce31df7
lysenko <nikita.lysenko@spryker.com>
2019-03-04 14:27:17 +0200

    feature/ECO-2504: CR Fix

--- src/SprykerEco/Yves/CrefoPay/Theme/default/components/molecules/crefopay-form-loader/index.ts
+++
@@ -1,3 +0,0 @@
-import './crefopay-form-loader.scss';
-import register from 'ShopUi/app/registry';
-export default register('crefopay-form-loader', () => import(/* webpackMode: "lazy" */'./crefopay-form-loader'));
